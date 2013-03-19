ffaa2a7523ac8b3849685f4aa9e9911ee13fc806
Francois <f.michaudon@gmail.com>
2013-03-19 18:14:22 +0100

    STEP2 Models et Collection

--- app/scripts/collections/article-collection.js
+++ app/scripts/collections/article-collection.js
@@ -2,6 +2,5 @@ gravityBlog.Collections.ArticleCollection = Backbone.Collection.extend({
 
 	model: gravityBlog.Models.ArticleModel,
 
-	url: 'http://localhost:1337/article/'
 
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,13 +8,25 @@ window.gravityBlog = {
 	init: function() {
 		console.log('| Gravity Blog Initialisation');
 
-		var articleList = new gravityBlog.Collections.ArticleCollection();
-		articleList.fetch();
+		console.log('| Models \n');
 
-		var view = gravityBlog.view = new gravityBlog.Views.applicationView({
-			collection : articleList,
-		});
-		view.debug();
+		var unArticle = new gravityBlog.Models.ArticleModel(); //defaut
+		console.log('unArticle.get(title)>>' , unArticle.get('title')); //getter
+		unArticle.set('title', 'un titre personalisé'); //setter
+		console.log("unArticle.set('title', 'un titre personalisé')");
+		console.log('unArticle.get(title)>>',unArticle.get('title'));
+		unArticle.set('subtitle','yeah ! '); //set a new attribut for this object
+		console.log('unArticle.attributes>>' , unArticle.attributes ,'\n');
+
+		var unAutre = new gravityBlog.Models.ArticleModel({title: 'Tuto Backbone', content: 'PART 1 : models'});
+
+		console.log('| Collection\n');
+
+		var articleList = new gravityBlog.Collections.ArticleCollection();
+		articleList.add(unArticle);
+		articleList.add([unAutre , {title:'Tricks', content:'on peut faire ça ! '}]);
+		console.log(articleList)
+
 	}
 };
 
