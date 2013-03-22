898447fd62ee6e26a44a644c685386eecef1b416
Francois <f.michaudon@gmail.com>
2013-03-22 19:57:13 +0100

    Step4

--- app/scripts/views/article-view.js
+++ app/scripts/views/article-view.js
@@ -5,15 +5,47 @@ gravityBlog.Views.articleView = Backbone.View.extend({
 
 	className: 'article-view',
 
+	template: _.template($('#article-template').html()),
+
+	events: {
+		'dblclick p,label': 'showEditTools',
+		'click .edit-article': 'save'
+	},
 
 	initialize: function() {
 		this.render();
 	},
 
 	render: function() {
-		this.$el.html('<label>'+this.model.get('title')+'</label>'+'<p>'+this.model.get('content')+'</p>');
+		this.$el.html(this.template(this.model.toJSON()));
 		return this;
 	},
 
+	save: function() {
+
+		var inputVal = this.$el.find('.edit-title').val();
+		var textareaVal = this.$el.find('.edit-content').val();
+
+		if (inputVal !== '' && textareaVal !== '') {
+			this.model.set({
+				'title': inputVal,
+				'content': textareaVal
+			});
+			this.hideEditTools();
+			this.render();
+		}
+	},
+
+	showEditTools: function() {
+		this.$el.find('label').hide();
+		this.$el.find('p').hide();
+		this.$el.find('.tools').show();
+	},
+
+	hideEditTools: function() {
+		this.$el.find('label').show();
+		this.$el.find('p').show();
+		this.$el.find('.tools').hide();
+	}
 
 });
